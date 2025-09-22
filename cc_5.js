043da5ceb64df45a6ecc4148ae9da46393d1c8ec
Austin Lee <austinlee@usf.edu>
2025-09-22 13:27:56 -0400

    script

--- cc_5.js
+++ cc_5.js
@@ -3,3 +3,4 @@ let workers=[
     {name:"Terry", hourlyRate:25.50, hoursWorked:25},                       
     {name:"Larry", hourlyRate:10.00, hoursWorked:40},
 ]
+//array complete for 3 employees
