f78e9e47623b054755c55d733dca760a0713f9ce
Austin Lee <austinlee@usf.edu>
2025-09-23 10:40:28 -0400

    check console logs

--- cc_5.js
+++ cc_5.js
@@ -4,19 +4,20 @@ let workers=[
     {name:"Larry", hourlyRate:10.00, hoursWorked:40},
 ]
 //array complete for 3 employees
-let hours= 40
+
 function calculateBasePay(rate, hours) {
   if (hours<=40) {
  return rate*hours;
   }
 }
+console.log(calculateBasePay)
 // function for regular pay
 function calculateOvertimePay(rate, hours){
     if (hours>40){
     return(rate*hours)+(hours-40 *1.5);}
 }
+console.log(calculateOvertimePay)
 // function for overtime pay
 function calculateTaxes(grossPay){
 
 }
-console.log()
