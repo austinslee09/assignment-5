94e975742157859fc203fe6ef3537ff712258452
Austin Lee <austinlee@usf.edu>
2025-09-24 10:29:31 -0400

    test

--- cc_5.js
+++ cc_5.js
@@ -1,24 +1,26 @@
 let workers=[
-    {name:"Dave", hourlyRate:40.00, hoursWorked:30},
-    {name:"Terry", hourlyRate:25.50, hoursWorked:25},                       
-    {name:"Larry", hourlyRate:10.00, hoursWorked:40},
+    {Name:"Dave", hourlyRate:40.00, hoursWorked:30},
+    {Name:"Terry", hourlyRate:25.50, hoursWorked:25},                       
+    {Name:"Larry", hourlyRate:10.00, hoursWorked:40},
 ]
 //array complete for 3 employees
 
-function calculateBasePay(rate, hours) {
+function calculateBasePay(hourlyRate, hoursWorked) {
   if (hours<=40) {
- return rate*hours;
+ return hourlyRate*hoursWorked;
   }
 }
-console.log(calculateBasePay)
+console.log(multiply(workers[0].hourlyRate, workers[0].hoursWorked));
 // function for regular pay
-function calculateOvertimePay(rate, hours){
+function calculateOvertimePay(hourlyRate, hoursWorked){
     if (hours>40){
-    return(rate*hours)+(hours-40 *1.5);}
-}
-console.log(calculateOvertimePay)
-// function for overtime pay
-function calculateTaxes(grossPay){
-return pay*.85
+    return(hourlyRate-40 *1.5)+(hourlyRate*hoursWorked);}
 }
 
+// function for overtime pay
+function calculateTaxes(grossPay){
+return grossPay*.15
+}
+function processPayroll(employee){
+  return {names:Name, }
+}
